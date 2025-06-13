1f939fc636a6bd5acfe6c4ce68e0c893ee36979b
Ausath Ikram <ausathdzil@gmail.com>
2025-06-13 21:58:41 +0700

    Add admin role, use bcrypt to hash password

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-import { HandPlatterIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import bcrypt from 'bcryptjs';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { nextCookies } from 'better-auth/next-js';
+import { admin } from 'better-auth/plugins';
 
 import { db } from '@/db';
 import * as schema from '@/db/schema';
@@ -12,10 +14,20 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+    /**
+     * Better Auth uses the scrypt algorithm to hash passwords by default. But for this project,
+     * I'm using bcrypt to demonstrate how to use a different hashing algorithm.
+     */
+    password: {
+      hash: async (password) => {
+        return await bcrypt.hash(password, 10);
+      },
+      verify: async ({ hash, password }) => {
+        return await bcrypt.compare(password, hash);
+      },
+    },
   },
-  plugins: [nextCookies()],
+  plugins: [admin(), nextCookies()],
 });
 
-const api = auth.api;
-
-export const { signInEmail, signUpEmail, getSession } = api;
+export const { signInEmail, signUpEmail, getSession } = auth.api;
