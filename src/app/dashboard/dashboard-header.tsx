a0cb8d28670cf8ace4a7bf202ae0cceb887adf1d
Ausath Ikram <ausathdzil@gmail.com>
2025-06-22 19:04:42 +0700

    Cursor pointer

--- src/app/dashboard/dashboard-header.tsx
+++ src/app/dashboard/dashboard-header.tsx
@@ -3,7 +3,7 @@ import { SidebarTrigger } from '@/components/ui/sidebar';
 export function DashboardHeader({ title }: { title: string }) {
   return (
     <header className="p-4 relative border-b">
-      <SidebarTrigger className="absolute left-4 top-1/2 -translate-y-1/2" />
+      <SidebarTrigger className="absolute md:hidden left-4 top-1/2 -translate-y-1/2" />
       <h1 className="font-semibold text-center">{title}</h1>
     </header>
   );
